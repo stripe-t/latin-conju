ed17d7302be33cae8b2ad4012ddadc043a8974c5
suitougreentea <suitougreentea@users.noreply.github.com>
2016-10-11 21:02:47 +0900

    First commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,30 @@
+import g from "gulp"
+import webpack from "webpack-stream"
+import pug from "gulp-pug"
+import stylus from "gulp-stylus"
+import livereload from "gulp-livereload"
+
+g.task("html", () => g.src("src/index.pug").pipe(pug()).pipe(g.dest("docs/")).pipe(livereload()))
+g.task("js",   () => g.src("src/index.js").pipe(webpack({
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: "babel" },
+      { test: /\.json$/, loader: "json" }
+    ]
+  },
+  output: {
+    filename: "index.js"
+  }
+})).pipe(g.dest("docs/")).pipe(livereload()))
+g.task("css",  () => g.src("src/index.stylus").pipe(stylus()).pipe(g.dest("docs/")).pipe(livereload()))
+g.task("font", () => g.src("src/font/**/*").pipe(g.dest("docs/font")))
+
+g.task("all", ["html", "js", "css", "font"])
+
+g.task("default", () => {
+  livereload.listen()
+  g.watch("src/index.pug", ["html"])
+  g.watch("src/**/*.js", ["js"])
+  g.watch("src/**/*.json", ["js"])
+  g.watch("src/**/*.stylus", ["css"])
+})
